f4845f3d7065ec46ef743a35b045fe97146d6b1c
Max Zhou <momoaolig@gmail.com>
2024-08-15 17:40:35 -0600

    add ss tools

--- code/SemanticScholar.py
+++ code/SemanticScholar.py
@@ -6,42 +6,14 @@ import os
 class SemanticScholar(BaseTool):
     name:str = "SemanticScholar"
     description:str = """
-    This tool is used for searching related papers according to given keywords. 
-    The input should be a string concatnating keywords with "+", e.g. "keyword1+keyword2+keyword3".
-    It will give back titles and abstracts of the top ranked 10 papers.
+    This tool is used for searching related papers. 
+    The input should the paper title. It is a string.
+    It will give back abstracts of the top 10 related papers.
     """
-    def _run(self, query):
-        SEMANTIC_SCHOLAR_API_URL = "https://api.semanticscholar.org/graph/v1/paper/search"
-        api_key=os.getenv("X_API_KEY")
-        headers = {
-            "x-api-key": api_key,  # Add your API key here if required
-            "Content-Type": "application/json",
-        }
-        
-        params = {
-            "query": query,
-            "fields": "title, abstract",
-            "limit": 10  # Number of results to retrieve
-        }
-        
-        response = requests.get(SEMANTIC_SCHOLAR_API_URL, headers=headers, params=params)
-        
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"Error: {response.status_code} - {response.text}")
-            return None
-
-# Function to search related papers
-class SS_Re(BaseTool):
-    name:str = "Recommendation"
-    description:str = """
-    This tool is used for getting recommendated papers by Semantic Scholars according to given paper id. 
-    The input should be a string.
-    It will give back titles and abstracts of the top ranked 10 papers.
-    """
-    def _run(self, paper_id):
-        SEMANTIC_SCHOLAR_API_URL = "https://api.semanticscholar.org/recommendations/v1/papers/forpaper"
+    def _run(self, title:str) -> list[str]:
+        match_url =  "https://api.semanticscholar.org/graph/v1/paper/search/match"
+        recommend_url = "https://api.semanticscholar.org/recommendations/v1/papers/forpaper/"
+        search_url = "https://api.semanticscholar.org/graph/v1/paper/search"
         api_key = os.getenv("X_API_KEY")
         if not api_key:
             print("API key is missing. Please set the X_API_KEY environment variable.")
@@ -49,19 +21,61 @@ class SS_Re(BaseTool):
             "x-api-key": api_key,
             "Content-Type": "application/json",
         }
-        
-        # Create the payload with the paper ID
-        params = {
-            "paper_id": paper_id,
-            "fields": "title, abstract",
-            "limit": 10,  # Number of recommendations to retrieve
+        match_params = {
+            "query": title,
+            "fields": "title",
+            "limit": 1
         }
-        
-        # Make the POST request to the API
-        response = requests.post(SEMANTIC_SCHOLAR_API_URL, headers=headers, params=params)
-        
-        if response.status_code == 200:
-            return response.json()
+        title_match_response = requests.get(match_url, headers=headers, params=match_params)
+                
+        if title_match_response.status_code == 200:
+            # test
+            matched_title = title_match_response.json()['data'][0]['title']
+            paperId = title_match_response.json()['data'][0]['paperId']
+            # print(type(paperId))
+            # print(title, paperId)
+
+            # Get the exact paper in SS
+            if matched_title == title:
+
+                url=recommend_url+paperId    
+                params = {
+                    "fields": "abstract",
+                    "limit": 10,  
+                }
+
+                # Search for Recommended Papers
+                response = requests.get(url, headers=headers, params=params)
+                # Get recommended Papers
+                if response.status_code == 200:
+                    recommended_papers = response.json()['recommendedPapers']
+
+                    # Recommended Papers could be None
+                    if recommended_papers:
+                        print(f'{len(recommended_papers)} founded')
+                        abstracts = [paper['abstract'] for paper in recommended_papers]
+                        # print(len(abstracts))
+                    else: 
+                        print('No related Paper!')
+                        return None
+                else:
+                    print(f"Error: {response.status_code} - {response.text}")
+                    return None
+            
+        # Not the same paper
         else:
-            print(f"Error: {response.status_code} - {response.text}")
-            return None
+            params = {
+                "query": title,
+                "fields": "abstract",
+                "limit": 10  # Number of results to retrieve
+            }
+
+            response = requests.get(search_url, headers=headers, params=params)
+            if response.status_code == 200:
+                entries = response.json()['data']
+                abstracts = [entry['abstract'] for entry in entries]
+                # print(len(abstracts))
+            else:
+                print(f"Error: {response.status_code} - {response.text}")
+                return None
+        return abstracts
