750f7946bffe9523930140363f3fda98bc69557a
MikeW-gc <91306553+MikeW-gc@users.noreply.github.com>
2024-08-15 17:30:18 -0600

    Add files via upload
    Newest version of the system:

--- code/agents_with_full_text.py
+++ code/agents_with_full_text.py
@@ -16,14 +16,15 @@ os.environ['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
 os.environ['AWS_DEFAULT_REGION'] = os.getenv('AWS_DEFAULT_REGION')
 
 
-# Define data path
-base_dir = '../data'
+# Define paths
+base_dir = 'data'
 file_paths = {
     'testing_paper': '2024.sdp-1.15.txt',
     'clarity_agent_system_prompts': 'clarity_agent_system_prompts.txt',
     'experiments_methodology_agent_system_prompts': 'experiments_methodology_agent_system_prompts.txt',
     'leader_system_prompts': 'leader_system_prompts.txt',
-    'manager_system_prompts': 'manager_system_prompts.txt'
+    'manager_system_prompts': 'manager_system_prompts.txt',
+    'leader_task_prompts': 'leader_task_prompts.txt',
 }
 
 full_paths = {key: os.path.join(base_dir, file_name) for key, file_name in file_paths.items()}
@@ -34,7 +35,7 @@ clarity_agent_system_prompts_path = full_paths['clarity_agent_system_prompts']
 experiments_methodology_agent_system_prompts_path = full_paths['experiments_methodology_agent_system_prompts']
 leader_system_prompts_path = full_paths['leader_system_prompts']
 manager_system_prompts_path = full_paths['manager_system_prompts']
-
+leader_task_prompts_path = full_paths['leader_task_prompts']
 
 # Read files
 def read_files(paths):
@@ -52,38 +53,32 @@ clarity_agent_system_prompts = file_contents['clarity_agent_system_prompts']
 experiments_methodology_agent_system_prompts= file_contents['experiments_methodology_agent_system_prompts']
 leader_system_prompts = file_contents['leader_system_prompts']
 manager_system_prompts = file_contents['manager_system_prompts']
+leader_task_prompts = file_contents['leader_task_prompts']
 
 # list_avail_models()
 model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0"
 llm = load_model(model_id)
 
 # Initialize tools
-# Set the paper path
-# paper_path = Your Path
-
-# A tool that helps agent read files (supposedly)
+paper_path = os.path.abspath("/Users/harry/Desktop/SOS/agent/crew_ai_agent_project/daniel_2024_paper.txt")
 paper_read_tool = FileReadTool(file_path=testing_paper_path)
-
-# A tool that helps agent do RAG text search (supposedly) 
 paper_search_tool = TXTSearchTool(txt = testing_paper_path)
 
 
 
-# Define agents
 experiments_methodology_agent = Agent(
     role='experiments_methodology_agent',
     goal="Help review a scientific paper, especially focusing the clarity of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
-    backstory= f"{experiments_methodology_agent_system_prompts}",
+    backstory=f"{experiments_methodology_agent_system_prompts}",
     cache=True,
     llm=llm,
     tools=[paper_read_tool, paper_search_tool],
-    
 )
 
 clarity_agent = Agent(
     role='clarity_agent',
     goal="Help review a scientific paper, especially the part assigned to you. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
-    backstory= f"{clarity_agent_system_prompts_path}",
+    backstory= f"{clarity_agent_system_prompts}",
     cache=True,
     llm=llm,
     tools=[paper_read_tool, paper_search_tool],
@@ -97,7 +92,6 @@ impact_agent = Agent(
     cache=True,
     llm=llm,
     tools=[paper_read_tool, paper_search_tool],
-    
 )
 
 reviewer_leader = Agent(
@@ -110,48 +104,45 @@ reviewer_leader = Agent(
     tools=[paper_read_tool, paper_search_tool]
 )
 
-# Define the manager agent
 manager = Agent(
     role='manager',
-    goal="To coordinate and manage the workflow of the review process",
+    goal="To coordinate and manage the workflow of the review process, help agents coomunicate by sending the responses from the agents to the reviewer_leader or other agents.",
     backstory=f"{manager_system_prompts}",
     cache=True,
     verbose=True,
     llm=llm,
 )
 
-
-# Define high-level Tasks
 leader_task = Task(
-    description="You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. Task: Write a list of feedback comments, similar to the suggestions a reviewer might make. In addition, focus on major comments rather than minor comments; major comments are important things that affect the overall impact of the paper, whereas minor comments are small things like grammar check. Be specific in your suggestions, including details about method or resource names and any particular steps the authors should follow. However, don't suggest things that are already included/addressed in the paper. Remember that you can collaborate if necessary. If you want to write comments and/or ask for additional ones, you may want to provide your original comments to that the agent knows what was going on. Your review comments should be specific and express an appropriate level of importance. A comment like 'the authors should add more details about the proposed methods, such as XXX' is bad because it is too generic. Even for a paper with a good method description it is always possible to add more details. This review process should follow the high-level plan you have created. Agent information: There are 4 agents in the group, including yourself. You are 'reviewer_leader', the other agents are 'clarity_agent', 'impact_agent', and 'experiments_methodology_agent'.Each agent has a specific focus in the review process, and you should coordinate their efforts to ensure that the review is thorough and comprehensive.",
-    expected_output="A final list of feedback comments resembling that of the peer-reviews for scientific papers.",
+    description=f"{leader_task_prompts}",
+    expected_output="A final feedback comments resembling that of the peer-reviews for scientific papers, achieved by aggregating the comments from other agents.",
     agent=reviewer_leader,
+    output_file="final_review.txt",
 )
 
 clarity_agent_tasks = Task(
-    description= "You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. Task: Help the reviewer leader by providing information concerning the clarity of the paper.  Informations about agents: There are 4 agents in the group, including yourself. You are 'clarity_agent', the other agents are 'impact_agent', 'experiments_methodology_agent', and 'reviewer_leader'.",
-    expected_output="A set of responses to the leaders' requestsd, and feedback on the clarity of the paper.",
+    description= "Task: Help the reviewer leader by writing feedbacks on the clarity of the paper. If the reviewer_leader ask you questions, you will also need to respond to the questions.  Informations about agents: There are 4 agents in the group, including yourself. You are 'clarity_agent', the other agents are 'impact_agent', 'experiments_methodology_agent', and 'reviewer_leader'. You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. Please don't message others agents a chunk of the paper as they will have means to locate relevant content in the paper. ",
+    expected_output="A series of messages to 'reviewer_leader', and eventually a list of feedback focused on the clarity of the paper.",
     agent= clarity_agent,
     context=[leader_task]
 )
 
 
 impact_agent_tasks = Task(
-    description= "You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. Task: Help the reviewer leader by providing information concerning your section of the paper. Informations about agents: There are 4 agents in the group, including yourself. You are 'impact_agent', the other agents are 'clarity_agent', 'experiments_methodology_agent', and 'reviewer_leader'.",
-    expected_output="A set of responses to the leaders' requestsd, and feedback on the impact of the paper.",
+    description= "Task: Help the reviewer leader by writing feedback on the impact of the paper. Informations about agents: There are 4 agents in the group, including yourself. You are 'impact_agent', the other agents are 'clarity_agent', 'experiments_methodology_agent', and 'reviewer_leader'. You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. ",
+    expected_output="A series of messages to 'reviewer_leader', and eventually  list of feedback focused on the impact of the paper",
     agent= impact_agent,
     context=[leader_task]
 )
 
 experiments_methodology_tasks = Task(
-    description= "You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. Task: Help the reviewer leader by providing information concerning your section of the paper.Informations about agents: There are 4 agents in the group, including yourself. You are 'experiments_methodology_agent', the other agents are 'clarity_agent', 'impact_agent', and 'reviewer_leader'.",
-    expected_output="A set of responses to the leaders' requestsd, and feedback on the experiments and methodology of the paper.",
+    description= "Task: Help the reviewer leader by wrtting feedbacks on the experiments_methodology of the paper. Informations about agents: There are 4 agents in the group, including yourself. You are 'experiments_methodology_agent', the other agents are 'clarity_agent', 'impact_agent', and 'reviewer_leader'. You can access the paper using 'paper_read_tool' and searching for specific things in the text using 'paper_search_tool' to avoid accessing the whole paper and focus on some specific section of the paper to avoid viewing the paper everytime, which is costly. ",
+    expected_output="A series of messages to 'reviewer_leader', and eventually a list of feedback focused on the experiments/methodology of the paper.",
     agent= experiments_methodology_agent,
     context=[leader_task]
 )
 
 
-# Set Crew and kickoff
 review_crew = Crew(
     tasks=[leader_task, clarity_agent_tasks, impact_agent_tasks, experiments_methodology_tasks],
     agents=[impact_agent,reviewer_leader, experiments_methodology_agent, clarity_agent],
