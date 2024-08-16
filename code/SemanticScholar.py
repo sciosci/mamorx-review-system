bee9940e53cfb9a66083f5d4c6e655bdce188476
Max Zhou <momoaolig@gmail.com>
2024-08-16 16:28:10 -0600

    novelty agent and prompts added

--- code/SemanticScholar.py
+++ code/SemanticScholar.py
@@ -6,11 +6,11 @@ import os
 class SemanticScholar(BaseTool):
     name:str = "SemanticScholar"
     description:str = """
-    This tool is used for searching related papers. 
-    The input should the paper title. It is a string.
-    It will give back abstracts of the top 10 related papers.
+    A tool that can be used to retrieve a paper's related papers.
+    input: a dictionary storing title under key 'title'
     """
-    def _run(self, title:str) -> list[str]:
+    def _run(self, argument: dict) -> list[str]:
+        title = argument['title']
         match_url =  "https://api.semanticscholar.org/graph/v1/paper/search/match"
         recommend_url = "https://api.semanticscholar.org/recommendations/v1/papers/forpaper/"
         search_url = "https://api.semanticscholar.org/graph/v1/paper/search"
@@ -37,10 +37,12 @@ class SemanticScholar(BaseTool):
 
             # Get the exact paper in SS
             if matched_title == title:
+                print('Paper found in Semantic Scholar!')
+                print('Calling recommendation system...')
 
                 url=recommend_url+paperId    
                 params = {
-                    "fields": "abstract",
+                    "fields": "title,abstract",
                     "limit": 10,  
                 }
 
@@ -52,8 +54,9 @@ class SemanticScholar(BaseTool):
 
                     # Recommended Papers could be None
                     if recommended_papers:
-                        print(f'{len(recommended_papers)} founded')
+                        print(f'{len(recommended_papers)} found')
                         abstracts = [paper['abstract'] for paper in recommended_papers]
+                        titles = [paper['title'] for paper in recommended_papers]
                         # print(len(abstracts))
                     else: 
                         print('No related Paper!')
@@ -64,18 +67,22 @@ class SemanticScholar(BaseTool):
             
         # Not the same paper
         else:
+            print('Current paper is not in Semantic Scholar!')
+            print('Searching by title...')
             params = {
                 "query": title,
-                "fields": "abstract",
+                "fields": "title,abstract",
                 "limit": 10  # Number of results to retrieve
             }
 
             response = requests.get(search_url, headers=headers, params=params)
             if response.status_code == 200:
                 entries = response.json()['data']
+                print(f'{len(entries)} papers found')
                 abstracts = [entry['abstract'] for entry in entries]
+                titles = [entry['title'] for entry in entries]
                 # print(len(abstracts))
             else:
                 print(f"Error: {response.status_code} - {response.text}")
                 return None
-        return abstracts
+        return titles, abstracts
