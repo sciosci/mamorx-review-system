ea183793fb789da1fb41c8cda589e7297ee59f8b
MikeW-gc <91306553+MikeW-gc@users.noreply.github.com>
2024-08-23 10:16:46 -0600

    Add files via upload
    Update MultiAgentWorkflow

--- ai_reviewer/MultiAgentWorkflow.py
+++ ai_reviewer/MultiAgentWorkflow.py
@@ -1,24 +1,20 @@
 import os
+import json
 from dotenv import load_dotenv
 from crewai import Agent, Task, Crew, Process
-from langchain_openai import ChatOpenAI
 from model import load_model
 from crewai_tools import FileReadTool, TXTSearchTool
-from SemanticScholar import SemanticScholar
 
-
-"""
-This is the class that manage a multi-agent workflow. Right now the agents are leader, clarity_agent, experiments_agent, and novelty_agent. The novelty agent is a placeholder
-for the actually implementation using crewai. The main system (to be implemented) should call the class to initiate a review.
-"""
 
 class MultiAgentWorkflow:
-    def __init__(self, base_dir='data', model_id="anthropic.claude-3-5-sonnet-20240620-v1:0"):
+    def __init__(self, base_dir='data', model_id="anthropic.claude-3-5-sonnet-20240620-v1:0", prompts_file='prompts.json'):
         self.base_dir = base_dir
         self.model_id = model_id
+        self.prompts_file = prompts_file
         self.load_environment()
         self.setup_file_paths()
         self.load_file_contents()
+        self.load_prompts()
         self.setup_tools()
         self.setup_agents()
         self.setup_tasks()
@@ -31,16 +27,16 @@ class MultiAgentWorkflow:
         ]
         for var in env_vars:
             os.environ[var] = os.getenv(var, '')
+
+    def load_prompts(self):
+        with open(self.prompts_file, 'r') as file:
+            self.prompts = json.load(file)
     
     def setup_file_paths(self):
         file_paths= {
             'testing_paper': '2024.sdp-1.15.txt',
-            'clarity_agent_system_prompts': 'clarity_agent_system_prompts.txt',
-            'experiments_methodology_agent_system_prompts': 'experiments_methodology_agent_system_prompts.txt',
-            'leader_system_prompts': 'leader_system_prompts.txt',
-            'manager_system_prompts': 'manager_system_prompts.txt',
-            'leader_task_prompts': 'leader_task_prompts.txt',
         }
+
         self.full_paths = {key: os.path.join(self.base_dir, file_name) for key, file_name in file_paths.items()}
 
     def load_file_contents(self):
@@ -53,7 +49,6 @@ class MultiAgentWorkflow:
     def setup_tools(self):
         self.paper_read_tool = FileReadTool(file_path=self.full_paths['testing_paper'])
         self.paper_search_tool = TXTSearchTool(txt=self.full_paths['testing_paper'])
-        self.related_paper_tool = SemanticScholar()
 
     def setup_agents(self):
         self.llm = load_model(self.model_id)
@@ -61,91 +56,91 @@ class MultiAgentWorkflow:
         self.review_leader = Agent(
             role='review_leader',
             goal="Lead the review of a scientific paper. Assign tasks to the other agents and answer their questions. Make sure the review is thorough and accurate.",
-            backstory=self.file_contents['leader_system_prompts'],
+            backstory=self.prompts['multi_agent_without_knowledge']['leader']['system_prompt'],
             cache=True,
             llm=self.llm,
             tools=[self.paper_read_tool, self.paper_search_tool],
+            verbose=True
             )
 
-        self.experiments_methodology_agent = Agent(
-            role='experiments_methodology_agent',
-            goal="Help review a scientific paper, especially focusing the experiments/methodology of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
-            backstory=self.file_contents['experiments_methodology_agent_system_prompts'],
+        self.experiments_agent = Agent(
+            role='experiments_agent',
+            goal="Help review a scientific paper, especially focusing the experiment/methods of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
+            backstory= self.prompts['multi_agent_without_knowledge']['experiment_agent']['system_prompt'],
             cache=True,
             llm=self.llm,
             tools=[self.paper_read_tool, self.paper_search_tool],
+            verbose=True,
             )
         
         self.clarity_agent = Agent(
             role='clarity_agent',
             goal="Help review a scientific paper, especially focusing the clarity of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
-            backstory=self.file_contents['clarity_agent_system_prompts'],
+            backstory=self.prompts['multi_agent_without_knowledge']['clarity_agent']['system_prompt'],
             cache=True,
             llm=self.llm,
             tools=[self.paper_read_tool, self.paper_search_tool],
+            verbose=True
             )
         
-        self.novelty_agent = Agent(
-            role='novelty_agent',
-            goal="Help review a scientific paper, especially focusing the novelty of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
-            backstory=self.file_contents['impact_system_prompts'],
+        self.impact_agent = Agent(
+            role='impact_agent',
+            goal="Help review a scientific paper, especially focusing the impact of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
+            backstory=self.prompts['multi_agent_without_knowledge']['impact_agent']['system_prompt'],
             cache=True,
             llm=self.llm,
-            tools=[self.paper_read_tool, self.paper_search_tool, self.related_paper_tool],
+            tools=[self.paper_read_tool, self.paper_search_tool],
+            verbose=True
             )
         
         self.manager = Agent(
             role='manager',
-            goal="To coordinate and manage the workflow of the review process, help agents coomunicate by sending the responses from the agents to the reviewer_leader or other agents.",
-            backstory=self.file_contents['manager_system_prompts'],
+            goal="Manage the workflow of the review process by bridging the communication between the agents.",
+            backstory=self.prompts['multi_agent_without_knowledge']['manager']['system_prompt'],
             cache=True,
             llm=self.llm,
-            tools=[self.paper_read_tool, self.paper_search_tool],
-            )
-        
-
-        def setup_tasks(self):
-            self.leader_task = Task(
-                description=self.file_contents['leader_task_prompts'],
-                expected_output='A set of messgages to other agents to orchestrate the review and a final feedback comments resembling that of the peer-reviews for scientific paper.',
-                agent=self.review_leader,
-                output_file='final_review.txt',
             )
 
-            self.clarity_agent_tasks = Task(
-                description=self.file_contents['clarity_agent_task_prompts'],
-                expected_output="A series of messages to 'reviewer_leader', and eventually a list of feedback focused on the clarity of the paper.",
-                agent=self.clarity_agent,
-                output_file="clarity_feedback.txt",
+    def setup_tasks(self):
+        self.leader_task = Task(
+            description=self.prompts['multi_agent_without_knowledge']['leader']['task_prompt'],
+            expected_output='A final list of feedbacks/comments for the paper resembling that of the peer-reviews for scientific paper, it should incorporates suggestions from the other expert agents.',
+            agent=self.review_leader,
+            output_file='aug_21_final_review_2.txt',
             )
 
-            self.experiments_methodology_agent_tasks = Task(
-                description=self.file_contents['experiments_methodology_agent_task_prompts'],
-                expected_output="A series of messages to 'reviewer_leader', and eventually a list of feedback focused on the methodology of the paper.",
-                agent=self.experiments_methodology_agent,
-                output_file="experiments_methodology_feedback.txt",
+        self.clarity_agent_tasks = Task(
+            description=self.prompts['multi_agent_without_knowledge']['clarity_agent']['task_prompt'],
+            expected_output="A series of messages sent to 'review_leader'.",
+            agent=self.clarity_agent,
+            context=[self.leader_task]
             )
 
-            self.impact_agent_tasks = Task(
-                description=self.file_contents['impact_system_prompts'],
-                expected_output="A series of messages to 'reviewer_leader', and eventually a list of feedback focused on the impact of the paper.",
-                agent=self.impact_agent,
-                output_file="impact_feedback.txt",
+        self.experiments_agent_tasks = Task(
+            description=self.prompts['multi_agent_without_knowledge']['experiment_agent']['task_prompt'],
+            expected_output="A series of messages sent to 'review_leader'.",
+            agent=self.experiments_agent,
+            context=[self.leader_task]
+            )
+
+        self.impact_agent_tasks = Task(
+            description=self.prompts['multi_agent_without_knowledge']['impact_agent']['task_prompt'],
+            expected_output="A series of messages sent to 'review_leader'.",
+            agent=self.impact_agent,
+            context=[self.leader_task]
             )
             
 
         
-        def setup_crews(self):
-            self.review_crew = Crew(
-                agents=[self.review_leader, self.experiments_methodology_agent, self.clarity_agent, self.impact_agent, self.manager],
-                takss=[self.leader_task, self.clarity_agent_tasks, self.experiments_methodology_agent_tasks, self.impact_agent_tasks],
-                process=Process.hierarchical,
-                memory=True,
-                manager_agent=self.manager,
-
+    def setup_crew(self):
+        self.review_crew = Crew(
+            agents=[self.review_leader, self.experiments_agent, self.clarity_agent, self.impact_agent],
+            tasks=[self.leader_task, self.clarity_agent_tasks, self.experiments_agent_tasks, self.impact_agent_tasks],
+            process=Process.hierarchical,
+            memory=True,
+            manager_agent=self.manager
             )
 
-        def initiate_workflow(self):
-            result = self.review_crew.kickoff()
-            return result
+    def initiate_workflow(self):
+        self.review_crew.kickoff()
 
