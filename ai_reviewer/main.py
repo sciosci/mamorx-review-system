db7edab26218769c00f0a2b457ccd8194f31d3ae
MikeW-gc <91306553+MikeW-gc@users.noreply.github.com>
2024-09-04 20:20:39 -0600

    Update main.py

--- ai_reviewer/main.py
+++ ai_reviewer/main.py
@@ -1,21 +1,22 @@
-from MultiAgentWorkflow import MultiAgentWorkflow
-
+from review_system_workflow import ReviewSystemWorkflow
+import os
 """ This would be the main function that initiate the PDF parsing processing and feed the data to the functions. This is just
     a skeleton of the function. Ideally the system should feed the text, titles, and other information as parameters for the 
     4 systems.
 """
 
+def main(base_dir, pdf_path, prompts_file, model_id):
+    # Initialize & run the ReviewSystemWorkflow
+    review_system = ReviewSystemWorkflow(base_dir, pdf_path, prompts_file, model_id)
+    result = review_system.run_workflow()
 
-def main():
-    # Instantiate the MultiAgentWorkflow
-    workflow = MultiAgentWorkflow(
-        base_dir='/Users/harry/Desktop/SOS/agent/crew_ai_agent_project',
-        model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
-        prompts_file='/Users/harry/Desktop/SOS/agent/crew_ai_agent_project/prompts.json'
-    )
-
-    # Initiate the workflow
-    workflow.initiate_workflow()
 
 if __name__ == "__main__":
-    main()
+    # Define parameters
+    BASE_DIR = '/'    # Project dir
+    PDF_PATH = '/'    # PDF path
+    PROMPTS_FILE = '/' # Prompt.json 
+    MODEL_ID = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+
+    # Call the main function with parameters
+    main(BASE_DIR, PDF_PATH, PROMPTS_FILE, MODEL_ID)
