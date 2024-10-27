4a9509d05019fa90f1b89541c662266774de9892
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-10-27 16:42:43 -0600

    Use ollama for multiagent review and disable baseline review generation

--- Mamorx/MAMORX/review_generator/multi_agent_reviewer.py
+++ Mamorx/MAMORX/review_generator/multi_agent_reviewer.py
@@ -1,14 +1,14 @@
 import os
 from typing import Optional
-from langchain_aws import ChatBedrock
-from crewai import Agent, Crew, Process
+from crewai import Agent, Crew, Process, LLM
 from crewai_tools import TXTSearchTool
 
-from crewai.llm import LLM
 
 from MAMORX.schemas import APIConfigs, MultiAgentPrompt, MultiAgentCrewReviewResult
 from MAMORX.custom_crewai.task import CustomTask
 from MAMORX.custom_crewai_tools import FileReadToolUTF8, TextContainerTool
+# from MAMORX.utils import load_chatbedrock_llm_model
+# from langchain.llms.ollama import Ollama
 
 
 class MultiAgentReviewerCrew(object):
@@ -17,6 +17,24 @@ class MultiAgentReviewerCrew(object):
     '''
     def __init__(self, api_config: APIConfigs):
         self.api_config = api_config
+
+        # Setup environment variables for anthropic bedrock
+        # ,
+        #     region=api_config["aws_default_region"]
+        # os.environ['LITELLM_LOG'] = 'DEBUG'
+
+        # self.llm = LLM(
+        #     model=api_config['anthropic_model_id'],
+        #     aws_access_key_id=api_config['aws_access_key_id'],
+        #     aws_secret_access_key=api_config['aws_secret_access_key'],
+        #     aws_region_name=api_config['aws_default_region']
+        # )
+        self.llm = LLM(
+            model="ollama/llama3", 
+            base_url="http://localhost:11434"
+        )
+        # self.llm = Client(host="http://localhost:11434")
+        # os.environ['LITELLM_LOG'] = 'DEBUG'
 
 
     def review_paper(
@@ -45,43 +63,48 @@ class MultiAgentReviewerCrew(object):
             goal="Lead the review of a scientific paper. Assign tasks to the other agents and answer their questions. Make sure the review is thorough and accurate.",
             backstory=prompts['leader']['system_prompt'],
             cache=True,
-            llm=self.api_config["openai_model_name"],
+            llm=self.llm,
             tools=common_agent_tools + ([figure_critic_tool, novelty_tool] if use_knowledge else []),
-            verbose=True
+            verbose=True,
+            debug_mode=True
             )
         experiments_agent: Agent = Agent(
             role='experiments_agent',
             goal="Help review a scientific paper, especially focusing the experiment/methods of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
             backstory=prompts['experiment_agent']['system_prompt'],
             cache=True,
-            llm=self.api_config["openai_model_name"],
+            llm=self.llm,
             tools=common_agent_tools + ([figure_critic_tool] if use_knowledge else []),
             verbose=True,
+            debug_mode=True
             )
         clarity_agent: Agent = Agent(
             role='clarity_agent',
             goal="Help review a scientific paper, especially focusing the clarity of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
             backstory=prompts['clarity_agent']['system_prompt'],
             cache=True,
-            llm=self.api_config["openai_model_name"],
+            llm=self.llm,
             tools=common_agent_tools + ([figure_critic_tool] if use_knowledge else []),
-            verbose=True
+            verbose=True,
+            debug_mode=True
             )
         impact_agent: Agent = Agent(
             role='impact_agent',
             goal="Help review a scientific paper, especially focusing the impact of the paper. Be ready to answer questions from the review_leader and look for answers from the text assigned to you.",
             backstory=prompts['impact_agent']['system_prompt'],
             cache=True,
-            llm=self.api_config["openai_model_name"],
+            llm=self.llm,
             tools=common_agent_tools + ([novelty_tool] if use_knowledge else []),
-            verbose=True
+            verbose=True,
+            debug_mode=True
             )
         manager: Agent = Agent(
             role='manager',
             goal="Manage the workflow of the review process by bridging the communication between the agents.",
             backstory=prompts['manager']['system_prompt'],
             cache=True,
-            llm=self.api_config["openai_model_name"],
+            llm=self.llm,
+            debug_mode=True
             )
 
         #  Tasks
@@ -112,24 +135,27 @@ class MultiAgentReviewerCrew(object):
             )
 
         #   Crew
-        print("Initializing crew")
-        # review_crew:Optional[Crew] = Crew(
-        #     agents=[review_leader, experiments_agent, clarity_agent, impact_agent],
-        #     tasks=[leader_task, clarity_agent_tasks, experiments_agent_tasks, impact_agent_tasks],
-        #     process=Process.hierarchical,
-        #     memory=True,
-        #     manager_agent=manager
-        #     )
+        review_crew:Optional[Crew] = Crew(
+            agents=[review_leader, experiments_agent, clarity_agent, impact_agent],
+            tasks=[leader_task, clarity_agent_tasks, experiments_agent_tasks, impact_agent_tasks],
+            process=Process.hierarchical,
+            memory=True,
+            manager_agent=manager
+            )
 
-        # # Kickoff Crew
-        # print("Starting kickoff")
-        # crew_output = review_crew.kickoff()
+        # Kickoff Crew
+        crew_output = review_crew.kickoff()
 
-        # print("Getting final result")
-        # final_result = MultiAgentCrewReviewResult(
-        #     crew=review_crew,
-        #     crew_output=crew_output
-        # )
-        final_result = MultiAgentCrewReviewResult()
+        # Load review from output file
+        with open(output_path, "r") as f:
+            generated_review = f.read()
+
+        final_result = MultiAgentCrewReviewResult(
+            crew=review_crew,
+            crew_output=crew_output,
+            review=generated_review,
+            usage_metrics=review_crew.usage_metrics
+        )
+        print(review_crew.usage_metrics, end="\n\n")
 
         return final_result
