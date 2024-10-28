49212d5fdd5c4f15f03328d56e767905792864af
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-10-28 17:51:41 -0600

    Modify placeholder for papermage service main.py with sample of ENV vars

--- Mamorx/services/papermage_service/main.py
+++ Mamorx/services/papermage_service/main.py
@@ -7,6 +7,7 @@ import logging
 from concurrent import futures
 from MAMORX.figure_critic_rpc import figure_critic_pb2
 from MAMORX.figure_critic_rpc import figure_critic_pb2_grpc
+import os
 
 
 class FigureCriticService(figure_critic_pb2_grpc.FigureCriticServicer):
@@ -15,6 +16,7 @@ class FigureCriticService(figure_critic_pb2_grpc.FigureCriticServicer):
     
 
 def serve():
+    print(os.environ["API_KEY"])
     port = "5001"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     figure_critic_pb2_grpc.add_FigureCriticServicer_to_server(FigureCriticService(), server)
@@ -22,6 +24,7 @@ def serve():
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
+
 
 if __name__ == "__main__":
     logging.basicConfig()
