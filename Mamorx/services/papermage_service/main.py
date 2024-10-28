0f2e97d089fae054dcd86c5d0792a9213558cb33
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-10-27 21:48:57 -0600

    Add gRPC placeholder for figure critic service

---
+++ Mamorx/services/papermage_service/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import grpc
+import logging
+
+from concurrent import futures
+from MAMORX.figure_critic_rpc import figure_critic_pb2
+from MAMORX.figure_critic_rpc import figure_critic_pb2_grpc
+
+
+class FigureCriticService(figure_critic_pb2_grpc.FigureCriticServicer):
+    def Hello(self, request, context):
+        return figure_critic_pb2.helloReply(msg=(request))
+    
+
+def serve():
+    port = "5001"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    figure_critic_pb2_grpc.add_FigureCriticServicer_to_server(FigureCriticService(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
