824b06312459c523fcdd38f9fb2e4a00c2b66a5d
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-10-30 00:01:09 -0600

    Add unit testing files

--- Mamorx/testing/ollama_client_tester.py
+++ Mamorx/testing/ollama_client_tester.py
@@ -5,6 +5,6 @@ import litellm
 response = completion(
     model="ollama/llama3.1", 
     messages=[{ "content": "respond in 20 words. who are you?","role": "user"}], 
-    api_base="http://192.168.1.117:11434"
+    api_base="http://localhost:11434"
 )
 print(response)
