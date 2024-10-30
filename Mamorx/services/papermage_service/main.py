cc22789e1eb97a5ce858cf9d591a2afe950da7fb
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-10-30 00:04:08 -0600

    Implement papermage service

--- Mamorx/services/papermage_service/main.py
+++ Mamorx/services/papermage_service/main.py
@@ -7,19 +7,19 @@ import logging
 from concurrent import futures
 from MAMORX.figure_critic_rpc import figure_critic_pb2
 from MAMORX.figure_critic_rpc import figure_critic_pb2_grpc
-import os
+from services.papermage_service.figure_critic_server import FigureCriticService
 
-
-class FigureCriticService(figure_critic_pb2_grpc.FigureCriticServicer):
-    def Hello(self, request, context):
-        return figure_critic_pb2.helloReply(msg=(request))
     
-
 def serve():
-    print(os.environ["API_KEY"])
     port = "5001"
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    figure_critic_pb2_grpc.add_FigureCriticServicer_to_server(FigureCriticService(), server)
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ("grpc.max_send_message_length", 20000000),
+            ('grpc.max_receive_message_length', 20000000)
+        ]
+    )
+    figure_critic_pb2_grpc.add_FigureCriticServiceServicer_to_server(FigureCriticService(), server)
     server.add_insecure_port("[::]:" + port)
     server.start()
     print("Server started, listening on " + port)
