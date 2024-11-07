ec2b06add03aa8aaaaa2c9abc433f7e861dffdc8
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2024-11-07 01:14:09 -0700

    Successfully reads runtime variable for mamorx service url

--- Mamorx/services/web_service/src/app/api/generate-review/route.ts
+++ Mamorx/services/web_service/src/app/api/generate-review/route.ts
@@ -1,4 +1,9 @@
 import { NextResponse } from 'next/server'
+// import { loadEnvConfig } from '@next/env';
+
+// const projectDir = process.cwd();
+// loadEnvConfig(projectDir);
+export const dynamic = "force-dynamic";
 
 export async function GET() { // request: NextRequest
     const mamorx_service_url = process.env.MAMORX_SERVICE_URL;
